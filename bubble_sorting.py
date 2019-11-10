d580abb99f86ce7849575a57d6f1d541a6ab098b
anjansaha5 <anjansaha5@gmail.com>
2019-11-10 23:31:29 +0530

    Modified bubble sort program

--- bubble_sorting.py
+++ bubble_sorting.py
@@ -1,17 +1,35 @@
+"""
+Bubble Sort Algorithm
+No of elements: n
+No of passes: n - 1
+No of comparision: 1+2+3+...+n = (n(n-1))/2 = O(n^2)
+No of comparision: 1+2+3+...+n = (n(n-1))/2 = O(n^2)
 
-def bubble_sort(llist):
-	"""
-	In bubble sort it changes the original list.
-	"""
-	length = len(llist)
-	for i in range(length):
-		for j in range(length - i - 1):
-			if llist[j] > llist[j+1]:
-				llist[j],llist[j+1] = llist[j+1],llist[j]
-	return llist
+Time Complexity:
+Minimum : O(n)
+Maximum: O(n^2)
 
-llist = [1,2,5,7,9,13,29,64,35,87,56,8,47,86,54]
-print(llist) #Before sorting
-bubble_sort(llist)
-print(llist) #After sorting
+Bubble Sort algorithm is Adaptive, Stable
+"""
+
+def bubbleSort(arr):
+	for i in range(len(arr) - 1):
+
+		# If the list is already sorted then, flag will remain zero. 
+		# No need to iterate again. Time complexity will be O(n)
+		flag = 0 
+		for j in range(len(arr) - 1 - i):
+			if arr[j] > arr[j+1]:
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				flag = 1
+		if flag == 0: #If flag is zero, means list is sorted
+			break
+	return arr
+
+unsorted_list = [1,5,3,7,4,98,25,75,34,6,19,55,22,88,46]
+print('Before Sorting: ', unsorted_list)
+sorted_list = bubbleSort(unsorted_list)
+print('After Sorting: ', sorted_list)
+
+
 
