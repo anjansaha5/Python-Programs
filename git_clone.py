be3bc9792e3476d3ebf1a89a254d4229f430cd4e
Anjan Saha <Anjan.Saha@Ltts.com>
2020-01-03 18:34:01 +0530

    Added few repository

--- git_clone.py
+++ git_clone.py
@@ -1,15 +1,18 @@
 from git import Repo
 import os
 
+# github url
+git_url = 'https://github.com/vivekanandpv/python-basics.git'
 
-git_url = 'https://github.com/vivekanandpv/py-book-store-project.git'
+# First took the path except .git and the split with '/'
+dir_name= os.path.splitext(git_url)[0].split('/')
 
-dir_name = os.path.basename(os.path.splitext(git_url)[0])
+# Creating a path joining current dir + git-repository + parent_folder + child_folder
+repo_dir = os.path.join(os.getcwd() , 'git-repository', dir_name[-2], dir_name[-1])
 
-repo_dir = os.path.join(os.getcwd() , dir_name)
-
-if dir_name in os.listdir(os.getcwd()):
-    repo_dir = repo_dir + '_1'
+# Incase child folder is present then we rename to folder by adding _1.
+# if dir_name[-1] in os.listdir(os.path.join(os.getcwd(),'git-repository', dir_name[-2])):
+#     repo_dir = repo_dir + '_1'
 
 try:
     Repo.clone_from(git_url, repo_dir, branch='master')
